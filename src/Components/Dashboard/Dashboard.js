ff80f6e8b59c4368732fdce6a9abd80937725ddc
jordanutz <jordanutz@outlook.com>
2019-08-05 20:01:51 -0600

    Additional styling to Dashboard - modules and menu bar

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,13 +4,29 @@ import {AuthContext} from '../../Context/AuthContext'
 
 import {Container, Row, Col} from 'react-bootstrap'
 
+// Images
+import Current from './assets/current.svg'
+import Expenses from './assets/expenses.svg'
+import Income from './assets/income.svg'
+import User from './assets/user.svg'
+import Calendar from './assets/calendar.svg'
+
 const Dashboard = () => {
 
   const {user} = useContext(AuthContext)
 
   const displayDashboard = user ? 
     <div className="Dashboard">
-        <section className="DashboardMenu"></section>
+        <section className="DashboardMenu">
+          <i class="fas fa-home"></i>
+          <i class="fas fa-play-circle"></i>
+          <i class="fas fa-signal"></i>
+          <i class="fas fa-pause-circle"></i>
+          <i class="fas fa-user"></i>
+          <i class="fas fa-power-off"></i>
+        
+
+        </section>
         <section className="DashboardMain">
           <Container>
             <Row className="DashboardDetails">
@@ -22,6 +38,8 @@ const Dashboard = () => {
             <Row>
               <Col xs={12} sm={12} md={6} lg={4}>
                 <section id="Balance" className="DashboardModule">
+                  <h2>$21</h2>
+                  <img src={Current} />
                   <section id="BalanceDarken" className="ModuleFooter">
                     <h3>Details</h3>
                   </section>
@@ -29,6 +47,8 @@ const Dashboard = () => {
               </Col>
               <Col xs={12} sm={12} md={6} lg={4}>
                 <section id="Income" className="DashboardModule">
+                  <h2>$136</h2>
+                  <img src={Income} />
                   <section id="IncomeDarken" className="ModuleFooter">
                     <h3>Details</h3>
                   </section>
@@ -36,6 +56,8 @@ const Dashboard = () => {
               </Col>
               <Col xs={12} sm={12} md={6} lg={4}>
                 <section id="Expenses" className="DashboardModule">
+                  <h2>$169</h2>
+                  <img src={Expenses} />
                   <section id="ExpensesDarken" className="ModuleFooter">
                     <h3>Details</h3>
                   </section>
@@ -43,6 +65,8 @@ const Dashboard = () => {
               </Col>
               <Col xs={12} sm={12} md={6} lg={8}>
                 <section id="SpendingGraph" className="DashboardModule">
+                  <img src={User} />
+                  <h2 id="SpendingHeader">37%</h2>
                   <section id="SpendingDarken" className="ModuleFooter">
                     <h3>Details</h3>
                   </section>
@@ -50,8 +74,10 @@ const Dashboard = () => {
               </Col>
               <Col xs={12} sm={12} md={12} lg={4}>
                 <section id="Goals" className="DashboardModule">
+                  <h2>8/19</h2>
+                  <img src={Calendar} />
                   <section id="GoalsDarken" className="ModuleFooter">
-                      <h3>Details</h3>
+                      <h3>View Calendar</h3>
                   </section>
                 </section>
               </Col>
@@ -67,9 +93,9 @@ const Dashboard = () => {
     </div>
 
   return (
-    <div>
-    {displayDashboard}
-    </div>
+    <React.Fragment>
+      {displayDashboard}
+    </React.Fragment>
   )
 }
 
