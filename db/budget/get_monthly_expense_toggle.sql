69b4edb1617e8a8fe808b8c5de3b13aef4cff6ec
jordanutz <jordanutz@outlook.com>
2019-09-12 18:15:57 -0600

    Ordering income and expense summary by ID

--- db/budget/get_monthly_expense_toggle.sql
+++ db/budget/get_monthly_expense_toggle.sql
@@ -1,3 +1,3 @@
 SELECT user_expense.id AS expense, date_posted, description, amount, calendar, category, icon FROM user_expense
 INNER JOIN expense_category ON user_expense.category_id = expense_category.id
-WHERE user_id = $1 AND calendar = $2 ORDER BY date_posted asc
+WHERE user_id = $1 AND calendar = $2 ORDER BY user_expense.id ASC
--- db/budget/get_monthly_income.sql
+++ db/budget/get_monthly_income.sql
@@ -1,3 +1,3 @@
 SELECT user_income.id AS income, date_posted, description, amount, calendar, category, icon FROM user_income 
 INNER JOIN income_category ON user_income.category_id = income_category.id
-WHERE user_id = $1 AND calendar = $2 ORDER BY date_posted DESC
+WHERE user_id = $1 AND calendar = $2 ORDER BY user_income.id DESC
